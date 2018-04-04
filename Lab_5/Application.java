7f417060625e60a87f6fe946960318287aeca1e0
Howrran <37328064+Howrran@users.noreply.github.com>
2018-04-05 01:28:42 +0300

    Add files via upload

---
+++ Lab_5/Application.java
@@ -0,0 +1,65 @@
+package OOP;
+
+public class Application {
+    public static void main(String[] args) {
+        Command command;
+
+        if (args == null || args.length == 0){
+            command = new Echo("Error");
+        } else if (args.length == 1 && args[0].equals("help")){
+            command = new Help();
+        } else if (args.length == 1 && args[0].equals("exit")){
+            command = new Exit();
+        } else if (args.length == 2 && args[0].equals("date") && args[1].equals("now")){
+            command = new Date();
+        } else if (args.length == 2 && args[0].equals("echo")){
+            command = new Echo(args[1]);
+        } else {
+            command = new Echo("Error");
+        }
+
+        command.execute();
+    }
+}
+
+package OOP;
+
+public class Exit implements Command{
+    @Override
+    public void execute() {
+        System.out.println("Goodbye!");
+    }
+}
+
+package OOP;
+
+public class Help implements Command{
+    @Override
+    public void execute() {
+        System.out.println("Help executed");
+    }
+}
+
+
+package OOP;
+
+public class Date implements Command{
+    @Override
+    public void execute() {
+        System.out.println(System.currentTimeMillis());
+    }
+}
+
+package OOP;
+
+public class Echo implements Command {
+    private String input = "";
+
+    public Echo(String text){
+        input = text;
+    }
+    @Override
+    public void execute() {
+        System.out.println(input);
+    }
+}
